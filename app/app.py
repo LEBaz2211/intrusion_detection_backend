73d1e39130e93db9acf52ea1ddd615bddfa76629
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2023-12-18 09:26:11 +0100

    Refactor event handling logic in app.py

--- app/app.py
+++ app/app.py
@@ -59,18 +59,26 @@ def on_message(client, userdata, message):
         if not db_service.table_is_empty("event_log", device_id) and event_data.get('status') in statuses:
             print('here')
             if db_service.get_latest_event_log_status(device_id) == event_data.get('status'):
+                print('here3')
                 socketio.emit(event_data.get('status'), json.dumps(device_id))
                 db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
                 if db_service.get_device_status(device_id) != 'INTRUDER_DETECTED':
                     db_service.update_device_status(device_id, event_data.get('status'))
             elif db_service.get_latest_event_log_status(device_id) in ['INACTIVE', 'RASPBERRY_TIMEOUT'] and event_data.get('status') == 'ACTIVE':
+                print('here1')
                 socketio.emit('DEVICE_OPERATIONAL', json.dumps(device_id))
                 db_service.log_event(device_id, 'DEVICE_OPERATIONAL', event_date, json.dumps(event_data))
                 db_service.update_device_status(device_id, 'ACTIVE')
             elif db_service.get_latest_event_log_status(device_id) == 'DEVICE_TIMEOUT' and event_data.get('status') == 'INACTIVE':
+
                 socketio.emit('DEVICE_RECONNECTED', json.dumps(device_id))
                 db_service.log_event(device_id, 'DEVICE_RECONNECTED', event_date, json.dumps(event_data))
                 db_service.update_device_status(device_id, 'INACTIVE')
+            elif event_data.get('status') != None:
+                print('here2')
+                db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
+                db_service.update_device_status(device_id, event_data.get('status'))
+
 
         elif not device:
             db_service.add_device(device_id, "unknown", "unknown", None, None, None , None)
@@ -78,7 +86,6 @@ def on_message(client, userdata, message):
             print('here2')
             db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
             db_service.update_device_status(device_id, event_data.get('status'))
-            time.sleep(0.2)
 
         db_service.remove_duplicate_event_logs()
     except Exception as e:
