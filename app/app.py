811aa0111f9edf87ebcd44e54c3369189cc17698
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2023-12-18 09:36:59 +0100

    Refactor event handling logic in app.py

--- app/app.py
+++ app/app.py
@@ -57,24 +57,24 @@ def on_message(client, userdata, message):
         statuses = ['INTRUDER_DETECTED', 'INACTIVE', 'RASPBERRY_TIMEOUT', 'ACTIVE', 'DEVICE_TIMEOUT']
 
         if not db_service.table_is_empty("event_log", device_id) and event_data.get('status') in statuses:
-            print('here')
             if db_service.get_latest_event_log_status(device_id) == event_data.get('status'):
-                print('here3')
                 socketio.emit(event_data.get('status'), json.dumps(device_id))
                 db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
                 if db_service.get_device_status(device_id) != 'INTRUDER_DETECTED':
                     db_service.update_device_status(device_id, event_data.get('status'))
             elif db_service.get_latest_event_log_status(device_id) in ['INACTIVE', 'RASPBERRY_TIMEOUT'] and event_data.get('status') == 'ACTIVE':
-                print('here1')
                 socketio.emit('DEVICE_OPERATIONAL', json.dumps(device_id))
                 db_service.log_event(device_id, 'DEVICE_OPERATIONAL', event_date, json.dumps(event_data))
-                db_service.update_device_status(device_id, 'ACTIVE')
+                db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
+                if db_service.get_device_status(device_id) != 'INTRUDER_DETECTED':
+                    db_service.update_device_status(device_id, event_data.get('status'))
             elif db_service.get_latest_event_log_status(device_id) == 'DEVICE_TIMEOUT' and event_data.get('status') == 'INACTIVE':
                 socketio.emit('DEVICE_RECONNECTED', json.dumps(device_id))
                 db_service.log_event(device_id, 'DEVICE_RECONNECTED', event_date, json.dumps(event_data))
-                db_service.update_device_status(device_id, 'INACTIVE')
+                db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
+                if db_service.get_device_status(device_id) != 'INTRUDER_DETECTED':
+                    db_service.update_device_status(device_id, event_data.get('status'))
             elif event_data.get('status') != None:
-                print('here2')
                 db_service.log_event(device_id, event_data.get('status'), event_date, json.dumps(event_data))
                 if db_service.get_device_status(device_id) != 'INTRUDER_DETECTED':
                     db_service.update_device_status(device_id, event_data.get('status'))
